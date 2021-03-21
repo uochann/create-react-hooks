6c5a02545bb22f3178e6bb0da651b97075a366db
魚崎柾秀 <uochann@uozakimasakishuunoMacBook-Pro.local>
2021-03-21 22:48:29 +0900

    複数のオブジェクトをまとめる

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react'
 
 const App = props => {
-  const [name, setName] = useState(props.name)
-  const [price, setprice] = useState(props.price)
+  const [state, setState] = useState(props)
+  const { name, price } = state
 
-  const reset = () => {
-    setprice(props.price)
-    setName(props.name)
-  }
-
-  return (
+   return (
     <>
       <p>現在の{name}の値段は、{price}円です</p>
-      <button onClick={() => setprice(price + 1)}>+1</button>
-      <button onClick={() => setprice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   )
 }
