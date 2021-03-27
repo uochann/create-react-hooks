787cf68d5e4af9c4ec7330a888722ac27400928c
魚崎柾秀 <uochann@uozakimasakishuunoMacBook-Pro.local>
2021-03-27 12:35:35 +0900

    feat:usecontext作成

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Event from './Event'
+import AppContext from '../contexts/AppContext'
 
-const Events = ({ state, dispatch}) => {
+const Events = () => {
+  const { state } = useContext(AppContext)
     return (
         <>
         <h4>イベント一覧</h4>
@@ -16,7 +18,7 @@ const Events = ({ state, dispatch}) => {
                 </tr>
             </thead>
             <tbody>
-            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+            {state.map((event, index) => (<Event key={index} event={event}/>))}
             </tbody>
         </table>
       </>
