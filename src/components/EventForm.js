b4f28378406bfb882f0ff0d4f38573e0f683c985
魚崎柾秀 <uochann@uozakimasakishuunoMacBook-Pro.local>
2021-03-28 23:35:34 +0900

    materialUI実践

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Box, ButtonGroup } from '@material-ui/core';
 
 import {
   CREATE_EVENT,
@@ -60,15 +60,21 @@ const EventForm = () => {
   const unCreatable = title === '' || body === ''
     return (
         <>
-        <h4>イベント作成フォーム</h4>
+        <Box fontSize="h4.fontSize" textAlign="center" m={4}>
+        イベント作成フォーム
+        </Box>
         <form className="classes.root" noValidate autoComplete="off">
           <TextField id="outlined-basic" label="タイトル" variant="outlined" id="formEventTitle"
                   value={title} onChange={e => setTitle(e.target.value)} /><br/>
           <TextField id="outlined-basic" label="ボディ" variant="outlined" id="formEventBody"
                   value={body} onChange={e => setBody(e.target.value)} /><br/>
-            <Button color="primary" variant="contained"  onClick={addEvent} disabled={unCreatable}>イベントを作成する</Button>
-            <Button color="secondary" variant="contained" onClick={deleteAllEvents} disabled={state.events.length === 0}>すべてのイベントを作成する</Button>
-            <Button color="secondary" variant="contained" onClick={deleteAllOperationLogs} disabled={state.operationLogs.lengs === 0}>すべてのログを削除する</Button>
+            <Box m={2}>
+              <ButtonGroup disableElevation variant="contained" color="primary" >
+                <Button color="primary" variant="contained"  onClick={addEvent} disabled={unCreatable}>イベントを作成する</Button>
+                <Button color="secondary" variant="contained" onClick={deleteAllEvents} disabled={state.events.length === 0}>すべてのイベントを作成する</Button>
+                <Button color="secondary" variant="contained" onClick={deleteAllOperationLogs} disabled={state.operationLogs.lengs === 0}>すべてのログを削除する</Button>
+              </ButtonGroup>
+            </Box>
         </form>
         </>
     )
